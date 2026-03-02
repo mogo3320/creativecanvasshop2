e7cb999a51e01501e0fbf80cbc8eda5890aabb71
mogo3320 <wazie3320@gmail.com>
2026-03-02 13:06:44 -0500

    Remove unused schema import from index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
-import * as schema from "./shared/schema.js";
 
 const app = express();
 app.use(express.json());
