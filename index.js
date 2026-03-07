642338441dcef0b1c239c823f3429cdde4feb8e3
mogo3320 <wazie3320@gmail.com>
2026-03-06 20:39:21 -0500

    Add test route and server listening log

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+// REQUIRED for Render
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port", process.env.PORT || 3000);
+});
